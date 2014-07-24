b705000ca5b1a95a21215bd7312c9eba46905cc4
Prashant <prashant@gyeser.com>
2014-07-24 15:32:03 +0530

    Added a script to push the gnip output to s3

---
+++ scripts/copy_gnip_output_to_s3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+now=$(date +"%Y-%m-%d-%S")
+newfilename="tweets.$now.csv"
+
+echo "The new file name is: $newfilename"
+
+echo "Copying files to s3..."
+
+s3cmd put gnip_output.csv "s3://gnip_output/$newfilename"
+
+echo "Removing local file..."
+
+rm tweet_collector_output.csv
